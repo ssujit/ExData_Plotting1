ddf79b7a26cbd6ae99356cea1ff09f283dc879a7
Sujit Kumar Sikder <sujiturp@yahoo.com>
2016-03-17 12:16:50 +0100

    Added files via upload

--- plot2.R
+++ plot2.R
@@ -11,32 +11,24 @@ list.files()
 
 # reading .txt data and attach in the R environment
 sourceData <- "./data/household_power_consumption.txt"
-plot2.data <-
-  read.table(
-    sourceData, sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = "."
-  )
+plot2.data <-read.table(sourceData, sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = ".")
 
 
 # subsetting data witin dates 2007-02-01 and 2007-02-0
 
-subsetplot2.data <-
-  plot2.data[plot2.data$Date %in% c("1/2/2007","2/2/2007"),]
+subsetplot2.data <- plot2.data[plot2.data$Date %in% c("1/2/2007","2/2/2007"),]
 
 # checking varriable class and convert to numeric
 names(subsetplot2.data)
 str(subsetplot2.data)
-globalActivepower <-
-  as.numeric(subsetplot2.data$Global_active_power)
+globalActivepower <- as.numeric(subsetplot2.data$Global_active_power)
 
 # covert data to required date and time format
+DateTime <- strptime(paste(subsetplot2.data$Date, subsetplot2.data$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 
-Date.Time <-
-  strptime(paste(subsetplot2.data, subsetplot2.data$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 
 
 # ploting trendline according to day after setting graphical devise
 png("plot2.png", width = 480, height = 480)
-plot(
-  Date.Time, globalActivepower, type = "1", xlab = "", ylab = "Global Active Power (kilowatts)"
-)
+plot(DateTime, globalActivepower, type = "l",  xlab = "", ylab = "Global Active Power(kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -11,26 +11,20 @@ list.files()
 
 # reading .txt data and attach in the R environment
 sourceData <- "./data/household_power_consumption.txt"
-plot4.data <-
-  read.table(
-    sourceData, sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = "."
-  )
+plot4.data <-read.table(sourceData, sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = ".")
 
 
 # subsetting data witin dates 2007-02-01 and 2007-02-0
 
-subsetplot4.data <-
-  plot4.data[plot4.data$Date %in% c("1/2/2007","2/2/2007"),]
+subsetplot4.data <- plot4.data[plot4.data$Date %in% c("1/2/2007","2/2/2007"),]
 
 # checking varriable class and convert to numeric
 names(subsetplot4.data)
 str(subsetplot4.data)
-globalActivepower <-
-  as.numeric(subsetplot4.data$Global_active_power)
+globalActivepower <- as.numeric(subsetplot4.data$Global_active_power)
 
 # covert variable to required date and time format
-Date.Time <-
-  strptime(paste(subsetplot4.data, subsetplot4.data$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+Date.Time <- strptime(paste(subsetplot4.data$Date, subsetplot4.data$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 
 
 # converting submeter variable in numeric
@@ -45,25 +39,12 @@ voltage <- as.numeric(subsetplot4.data$Voltage)
 
 # ploting trendline according to day after setting graphical devise
 png("plot4.png", width = 480, height = 480)
-plot(
-  Date.Time, globalActivepower, type = "l", xlab = "", ylab = "Global Active Power", cex =
-    0.2
-)
-
+par(mfrow = c(2, 2)) 
+plot(Date.Time, globalActivepower, type = "l", xlab = "", ylab = "Global Active Power", cex =0.2)
 plot(Date.Time, voltage, type = "l", xlab = "Date Time", ylab = "Voltage")
-
-plot(
-  Date.Time, subMetering1, type = "l", ylab = "Energy Submetering", xlab =
-    ""
-)
+plot(Date.Time, subMetering1, type = "l", ylab = "Energy Submetering", xlab ="")
 lines(Date.Time, subMetering2, type = "l", col = "red")
 lines(Date.Time, subMetering3, type = "l", col = "blue")
-legend(
-  "topright", c("Sub-metering-1", "Sub_metering_2", "Sub_metering_3"), lty =
-    , lwd = 2.5, col = c("black", "red", "blue"), bty = "o"
-)
-
-plot(
-  Date.Time, globalReactivepower, type = "l", xlab = "Date Time", ylab = "Global reactive power"
-)
+legend("topright", c("Sub-metering-1", "Sub_metering_2", "Sub_metering_3"), lty =, lwd = 2.5, col = c("black", "red", "blue"), bty = "o")
+plot(Date.Time, globalReactivepower, type = "l", xlab = "Date Time", ylab = "Global reactive power")
 dev.off()
